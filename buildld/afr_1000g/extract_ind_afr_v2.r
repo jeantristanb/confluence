46a4981d5e98fa4f70e5d9d6a9297d42986cc0a8
jeantristanb <jeantristanb@free.fr>
2023-11-23 12:55:43 +0200

    update readme

--- buildld/afr_1000g/extract_ind_afr_v2.r
+++ buildld/afr_1000g/extract_ind_afr_v2.r
@@ -1,11 +1,13 @@
 info_pop<-read.csv('superPop.info', sep='\t', header=F)
 info_ind<-read.csv('20130606_g1k.ped', sep='\t', header=T)
+bimfile=read.table('1000g_plk.bim')
 
 
 info_pop<-info_pop[,1:3];names(info_pop)<-c('Population', 'Description', 'SuperPop')
 info_ind<-merge(info_ind,info_pop,all=F, by='Population')
+info_ind<-info_ind[info_ind$Individual.ID %in% bimfile$V1,]
 info_ind_afr<-info_ind[info_ind$SuperPop=='AFR',]
 # deleted individual where mother or father are in data base
 balise_other<-(info_ind_afr$Paternal.ID!="0" & (info_ind_afr$Paternal.ID %in% info_ind_afr$Individual.ID)) | 
 (info_ind_afr$Maternal.ID!="0" & (info_ind_afr$Maternal.ID %in% info_ind_afr$Individual.ID)) 
-write.table(info_ind_afr[!balise_other,c('Individual.ID','Individual.ID')], file='list_afr_noappar.ind',sep="\t",row.names=F, col.names=F,quote=F)
+write.table(info_ind_afr[!balise_other,c('Individual.ID','Individual.ID')], file='list_afr_noappar_v2.ind',sep="\t",row.names=F, col.names=F,quote=F)
